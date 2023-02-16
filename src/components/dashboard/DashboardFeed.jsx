86d0a52cedcc46808ab1c6564e4a6a4242cf74f9
Thananat Saleewong <88187929+ThananatSaleewong@users.noreply.github.com>
2023-02-16 20:02:31 +0700

    END

--- src/components/dashboard/DashboardFeed.jsx
+++ src/components/dashboard/DashboardFeed.jsx
@@ -5,8 +5,9 @@ import { getImageURL, copyUrl } from "../../lib/utils";
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Dropdown, Spin } from "antd";
+import { Dropdown, Pagination, Spin } from "antd";
 import { EllipsisOutlined } from "@ant-design/icons";
+import { useLocation } from "react-router-dom";
 
 const items = [
   {
@@ -27,6 +28,12 @@ const items1 = [
 ];
 
 export default function DashboardFeed(props) {
+  const location = useLocation();
+  console.log(location.search);
+
+  const searchParams = new URLSearchParams(document.location.search);
+  console.log(searchParams.get("p"));
+
   const { currentUser } = props;
   const data = {
     name: "test",
@@ -35,21 +42,27 @@ export default function DashboardFeed(props) {
     date: "2022-01-01 10:00:00.123Z",
     image: "null",
   };
-  
+
   const [imageList, setImageList] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState({ page: 1, pageSize: 12 });
 
   useEffect(() => {
     fetchImageData();
-  }, []);
-
+  }, [page]);
+  console.log(page);
   const openInNewTab = (url) => {
     window.open(url, "_blank", "noreferrer");
   };
 
   const fetchImageData = async (event) => {
+    setLoading(true);
     try {
-      const resultList = await pb.collection("upload").getList(1, 30);
+      const resultList = await pb
+        .collection("upload")
+        .getList(page.page, page.pageSize, {
+          sort: "-created",
+        });
       setImageList(resultList);
     } catch (err) {
       toast.success(err, {
@@ -63,28 +76,26 @@ export default function DashboardFeed(props) {
         theme: "dark",
       });
     }
+    setLoading(false);
   };
 
   const handleDeleteImage = async (targetImg) => {
     const deleteImg = await pb.collection("upload").delete(targetImg);
     window.location.reload();
   };
-
+  console.log(imageList);
   async function handleChange(event) {
     const formData = new FormData();
     formData.append("image", event.target.files[0]);
     formData.append("title", event.target.files[0].name);
     formData.append("uploader", currentUser?.model.id);
     formData.append("email", currentUser?.model.email);
-
     setLoading(true);
     try {
       const record = await pb.collection("upload").create(formData);
-
       //1. Diable button
       //2. Change button content to Loading blah blah
       window.location.reload();
-      // const deleteImg = await pb.collection("upload").delete("");
     } catch (e) {
       alert(e);
     }
@@ -111,6 +122,14 @@ export default function DashboardFeed(props) {
     }
   };
 
+  const onChangePagination = (page, pageSize) => {
+    setPage({ ...page, page: page, pageSize: pageSize });
+  };
+
+  const onChangePaginationSize = (current, size) => {
+    setPage({ ...page, page: 1, pageSize: size });
+  };
+
   return (
     <div className="p-4 space-y-4">
       {/* Dropzone file upload */}
@@ -129,7 +148,7 @@ export default function DashboardFeed(props) {
         </label>
 
         {/* Image grid loop */}
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
           {imageList?.items.map((data, index) => (
             <div
               key={index}
@@ -146,7 +165,7 @@ export default function DashboardFeed(props) {
                 <img
                   src={getImageURL(data.collectionId, data.id, data.image, 100)}
                   alt=""
-                  className="w-12 h-12 mr-2"
+                  className="w-36 h-36 mr-2"
                 />
                 <div className="">
                   <p className="font-semibold text-sm">{data.title}</p>
@@ -180,6 +199,14 @@ export default function DashboardFeed(props) {
         </div>
       </Spin>
       <ToastContainer />
+      <Pagination
+        pageSize={page.pageSize}
+        pageSizeOptions={[12, 24, 48]}
+        total={imageList?.totalItems}
+        showSizeChanger
+        onChange={onChangePagination}
+        onShowSizeChange={onChangePaginationSize}
+      />
     </div>
   );
 }
